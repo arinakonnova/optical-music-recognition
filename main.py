f8f12815c4f4145dc55562c22e01e13c1d2ca3f9
Amélien <amelien.mdya@gmail.com>
2025-12-09 23:18:52 +0100

    insh'allah

--- main.py
+++ main.py
@@ -5,44 +5,31 @@ import numpy as np
 # importing sheet file
 img_file = sys.argv[1]
 img = cv2.imread(img_file)
-cv2.imshow("aaa", img)
+#cv2.imshow("aaa", img)
 
 # importing template
-staff_template = cv2.imread("./ressource/clef.png")
-cv2.imshow("template", staff_template)
 
-# # get template size
-# h, w = staff_template.shape[:2]
+template = cv2.imread("./ressource/quarter.png")
+cv2.imshow("template", template)
 
-# # finding matches (correct function)
-# print("Matching Finding staffs...")
-# res = cv2.matchTemplate(img, staff_template, cv2.TM_CCOEFF_NORMED)
-# print("Done!")
-
-# # location of best match
-# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-# # draw rectangle
-# top_left = max_loc
-# bottom_right = (top_left[0] + w, top_left[1] + h)
-# cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
-
-
-# Convert to grayscale
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-template_gray = cv2.cvtColor(staff_template, cv2.COLOR_BGR2GRAY)
-
-h, w = template_gray.shape[:2]
+h, w = template.shape[:2]
 
 # Template matching
-result = cv2.matchTemplate(img, staff_template, cv2.TM_CCOEFF_NORMED)
+result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
 
 # Find locations above threshold
-# try threshold: 0.3 forr clefs, 0.45 for quarters seems good ! #win
-locations = np.where(result >= 0.3)
+# try threshold: 
+# 0.3 for clefs, 
+# 0.45 for quarter & halfs seems good ! #win
+# 0.55 for sharps
+# whole spaces = not really working
+
+t=0.45
+
+locations = np.where(result >= t)
 
 # print number of matches
-print(len(locations[0]))
+#print(len(locations[0]))
 
 # To avoid drawing many overlapping rectangles, group nearby detections
 rectangles = []
@@ -59,7 +46,12 @@ for (x, y, w, h) in rectangles:
 # Save result
 
 cv2.imshow("Matchtemplate", result)
-cv2.imshow("Staffs", img)
+
+#resize
+h, w = img.shape[:2]
+scale = 800 / w   # target width = 1000px
+img = cv2.resize(img, (int(w * scale), int(h * scale)))
+cv2.imshow(str(t), img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
